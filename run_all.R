ac6c4631f0fdaad1601fc4d294d7ab496cc7b27c
alharry <alastair.harry@gmail.com>
2024-01-28 23:12:19 +0800

    changes

--- run_all.R
+++ run_all.R
@@ -11,10 +11,10 @@ source(here::here("code", "empirical.R"))
 
 
 # Generate files
-render("rmds/manuscript.Rmd", output_format = "all", output_dir = "reports", envir = new.env())
-render("rmds/figures.RmD", output_format = "all", output_dir = "reports", envir = new.env())
-render("rmds/figures-supp.RmD", output_format = "all", output_dir = "reports", envir = new.env())
-render("rmds/tables.RmD", output_format = "all", output_dir = "reports", envir = new.env())
+render("rmds/manuscript.Rmd", output_format = "all", output_file = "main-text.pdf", output_dir = "reports", envir = new.env())
+render("rmds/figures.RmD", output_format = "all", output_file = "figs.pdf", output_dir = "reports", envir = new.env())
+render("rmds/figures-supp.RmD", output_format = "all", output_file = "fig-supp.pdf", output_dir = "reports", envir = new.env())
+render("rmds/tables.RmD", output_format = "all", output_file = "tabs.pdf", output_dir = "reports", envir = new.env())
 
 ## Zotero citation key format
 # [auth:lower]_[Title:lower:skipwords:select,1,1]_[year]
